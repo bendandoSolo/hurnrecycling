4a3e2ebbdea5d7dd76393bbdb68707c3292d8ad7
ben roberts <bendando@hotmail.co.uk>
2021-10-06 11:53:28 +0100

    homepage image alignment done

--- components/Header.js
+++ components/Header.js
@@ -9,10 +9,8 @@ const size =  full ? "70vh" : "35vh";
 
   return (
     <header style={{marginTop: '100px'}}>
-      <div /*id="intro"*/ className="text-center bg-image" style={{height: size }}>
+      <div className="text-center bg-image" style={{height: size }}>
         <div
-
-          /*id="intro"*/
           className="text-center bg-image"
           style={{ backgroundImage: `url(${background})`, height: size }}
         >
@@ -49,3 +47,49 @@ const size =  full ? "70vh" : "35vh";
 };
 
 export default Header;
+
+
+export const HomeHeader = ({ background, text, smallText, button, mask, full }) => {
+
+  const showMask = !mask ? "mask" : "";
+  
+  const size =  full ? "70vh" : "35vh";
+  
+    return (
+      <header style={{marginTop: '100px'}}>
+        <div className="text-center bg-image" style={{height: size }}>
+          <div
+            className="text-center bg-image"
+            style={{ backgroundImage: `url(${background})`, height: size, backgroundPosition: 'bottom center' }}
+          >
+            <div className={showMask}>
+              <div className="d-flex justify-content-center align-items-center h-100 ">
+                <div style={{position: 'absolute', bottom: '30px'}}>
+                  <h1
+                    className="display-1"
+                    style={{
+                      marginBottom: "24px",
+                    }}
+                  >
+                    <strong>{text}</strong>
+                  </h1>
+                  {/* <hr className="my-4" /> */}
+                  <h4 className="font-weight-light mb-4 px-2">{smallText}</h4>
+                  {!!button && (
+                    <Link href="/contact">
+                      <button
+                        className="btn btn-lg btn-success mt-2"
+                        id="green-btn"
+                      >
+                        Get A Quote
+                      </button>
+                    </Link>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  };
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import Head from "next/head";
 import Services from "../components/Services";
 import About from "../components/About";
 import Clients from "../components/Clients";
-import Header from "../components/Header";
+import Header, {HomeHeader} from "../components/Header";
+
+//we are restyling this here
 
 export default function Home() {
   return (
@@ -13,9 +15,9 @@ export default function Home() {
         <title>Hurn Recycling</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header
+      <HomeHeader
         full
-        background="/images/Desktop/home.png"
+        background="/images/Desktop/Home.png"
         text="Total Waste Management Solutions"
         smallText="Helping local businesses since 1995"
         button
